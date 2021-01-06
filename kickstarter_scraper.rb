dd7b62e8ffc71214035ebb422d1ea284607cba12
Gerald <geraldarzola@gmail.com>
2021-01-05 20:38:07 -0500

    commit tree 9f6eed60031a77627b963604d6c3e3335c5fd3ee

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,10 @@
 # require libraries/modules here
+require "nokogiri"
+require 'pry'
+
 
 def create_project_hash
-  # write your code here
+ 
+  kickstarter = Nokogiri::HTML(File.read('fixtures/kickstarter.html'))
+  binding.pry
 end
